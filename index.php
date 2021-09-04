c4d177f744ee760598f989a84e4c3ff82f7e8516
Iszuddin Ismail <dino@websegera.my>
2021-09-04 11:58:27 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+* This file should be a secure protected file, which cannot be accessed unless the user
+* has successfully logged in with his/her credentials from users.txt.
+* 
+* This file should display a dashboard with data from the northwind database
+*/
